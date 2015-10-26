c1f8e69cb5d98f7efd6be58456bdc9efd213628d
George Song <george@55minutes.com>
2015-10-26 07:42:29 -0700

    Update packages

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ System.config({
     "angular-messages": "github:angular/bower-angular-messages@1.4.7",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.15",
     "angular-ui-router-title": "npm:angular-ui-router-title@0.0.4",
-    "babel": "npm:babel-core@5.8.25",
-    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "babel": "npm:babel-core@5.8.29",
+    "babel-runtime": "npm:babel-runtime@5.8.29",
     "clean-css": "npm:clean-css@3.4.6",
     "core-js": "npm:core-js@1.2.3",
     "css": "github:systemjs/plugin-css@0.1.19",
@@ -90,7 +90,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.25": {
+    "npm:babel-runtime@5.8.29": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:buffer@3.5.1": {
